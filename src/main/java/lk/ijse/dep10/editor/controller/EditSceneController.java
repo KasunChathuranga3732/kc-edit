63f0721bbf4b8b163af4aa1f777766fa2b549b3d
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-03 12:27:30 +0530

    Create UI and build its controller class

---
+++ src/main/java/lk/ijse/dep10/editor/controller/EditSceneController.java
@@ -0,0 +1,171 @@
+package lk.ijse.dep10.editor.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class EditSceneController {
+
+    @FXML
+    private Button btnDown;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnMinimize;
+
+    @FXML
+    private Button btnReplace;
+
+    @FXML
+    private Button btnReplaceAll;
+
+    @FXML
+    private Button btnResizable;
+
+    @FXML
+    private Button btnUp;
+
+    @FXML
+    private CheckBox chkMatchCase;
+
+    @FXML
+    private Label lblBytes;
+
+    @FXML
+    private Label lblCharactors;
+
+    @FXML
+    private Label lblResult;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblWords;
+
+    @FXML
+    private MenuItem mnSave;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TextArea txtEditor;
+
+    @FXML
+    private TextField txtFind;
+
+    @FXML
+    private TextField txtReplace;
+
+    @FXML
+    void btnDownOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnExitOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMinimizeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReplaceAllOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReplaceOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResizableOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnAboutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnCloseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnOpenOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnPrintOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnSaveAsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rootOnDragDropped(DragEvent event) {
+
+    }
+
+    @FXML
+    void rootOnDragOver(DragEvent event) {
+
+    }
+
+    @FXML
+    void rootOnMouseDragged(MouseEvent event) {
+
+    }
+
+    @FXML
+    void rootOnMousePressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void rootOnMouseReleased(MouseEvent event) {
+
+    }
+
+}
